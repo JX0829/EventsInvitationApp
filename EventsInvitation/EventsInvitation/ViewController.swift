b01702b99a7b69c5e7547637f47dd73bf207f43d
建鑫 <shenpuxinxi@MacBook-Pro-2.local>
2017-03-16 11:01:28 +0800

    项目初建

---
+++ EventsInvitation/EventsInvitation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EventsInvitation
+//
+//  Created by 建鑫 on 2017/3/16.
+//  Copyright © 2017年 CodeJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
