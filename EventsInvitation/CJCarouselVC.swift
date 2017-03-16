a8014c8449e6d236f844552158e8c06cb68b4016
建鑫 <shenpuxinxi@MacBook-Pro-2.local>
2017-03-16 15:37:20 +0800

    轮播创建

---
+++ EventsInvitation/CJCarouselVC.swift
@@ -0,0 +1,24 @@
+//
+//  CJCarouselVC.swift
+//  EventsInvitation
+//
+//  Created by 建鑫 on 2017/3/16.
+//  Copyright © 2017年 CodeJ. All rights reserved.
+//
+
+import UIKit
+
+class CJCarouselVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
